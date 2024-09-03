871731c564fbade027b34c230142318d658d7c98
Hudz-Roman <hudzrom@gmail.com>
2024-09-03 14:30:39 +0000

    Deploying to gh-pages from @ Hudz-Roman/goit-js-hw-10@7b90addc051754fe857c874884ca749cc42c0185 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";function i(o,e){fulfillEl}i(2,1500).then(({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)}).catch(({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)});
+//# sourceMappingURL=2-snackbar.js.map
