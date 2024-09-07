a51c0cd4d171480246f462375a72ad4437e572c3
Roman Hudz <hudzrom@gmail.com>
2024-09-07 23:01:10 +0300

    Add clearInterval for timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,6 +10,8 @@ const hoursDisp = document.querySelector('[data-hours]');
 const minutesDisp = document.querySelector('[data-minutes]');
 const secondsDisp = document.querySelector('[data-seconds]');
 
+let countdownInterval;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -36,7 +38,11 @@ flatpickr(dateTimePicker, options);
 startButton.addEventListener('click', () => {
   const selectedDate = new Date(dateTimePicker.value);
 
-  const countdownInterval = setInterval(updateCountdown, 1000);
+  if (countdownInterval) {
+    clearInterval(countdownInterval);
+  }
+
+  countdownInterval = setInterval(updateCountdown, 1000);
 
   function updateCountdown() {
     const currentTime = new Date().getTime();
@@ -53,19 +59,14 @@ startButton.addEventListener('click', () => {
     }
 
     function convertMs(ms) {
-      // Number of milliseconds per unit of time
       const second = 1000;
       const minute = second * 60;
       const hour = minute * 60;
       const day = hour * 24;
 
-      // Remaining days
       const days = Math.floor(ms / day);
-      // Remaining hours
       const hours = Math.floor((ms % day) / hour);
-      // Remaining minutes
       const minutes = Math.floor(((ms % day) % hour) / minute);
-      // Remaining seconds
       const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
       return { days, hours, minutes, seconds };
